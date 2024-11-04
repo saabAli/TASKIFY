7ed86b872d100c8d4081257b43719c1de1bd283c
saabAli <sabarincali6565@gmail.com>
2024-11-04 03:14:41 -0900

    set

--- utils/fileHandler.js
+++ utils/fileHandler.js
@@ -2,3 +2,7 @@ const fs = require('path');
 const path = require('path');
 
 const filepath = './data/tasks.json';
+
+exports.writeTaskToFile = (task) => {
+    fs.writeFilesync(filepath,  JSON.stringify(tasks, null, 2))
+}
